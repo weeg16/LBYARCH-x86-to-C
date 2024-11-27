a65b74a11d224c05e52a10f239a518c57cc90a4c
luis miguel <157296084+weeg16@users.noreply.github.com>
2024-11-27 22:41:48 +0800

    added manual input option

--- Cmp2.c
+++ Cmp2.c
@@ -1,47 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h> // Include this for time() function
+#include <time.h>
 #include <windows.h>
 
 extern void imgCvtGrayInttoFloat(int height, int width, int* intImg, float* floatImg);
 
 int main() {
-    int height, width, i, j;
-    printf("Input height and width: (separate w/ space)\n");
+    int height, width, i, j, choice;
+
+    printf("Input height and width (separate with space):\n");
     scanf("%d %d", &height, &width);
 
     int* intImg = (int*)malloc(height * width * sizeof(int));
     float* floatImg = (float*)malloc(height * width * sizeof(float));
 
-    // Seed the random number generator with the current time
-    srand((unsigned int)time(NULL));
+    printf("\nChoose input method:\n");
+    printf("1. Randomly generate pixel values\n");
+    printf("2. Manually input pixel values\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
 
-    // Generate random pixel values between 0 and 255
-    printf("\nGenerated pixel values:\n");
-    for (i = 0; i < height * width; i++) {
-        intImg[i] = rand() % 256; 
-        printf("%d ", intImg[i]);
-        if ((i + 1) % width == 0) {
-            printf("\n");
+    if (choice == 1) {
+        srand((unsigned int)time(NULL));
+
+        printf("\nGenerated pixel values:\n");
+        for (i = 0; i < height * width; i++) {
+            intImg[i] = rand() % 256;
+            printf("%d ", intImg[i]);
+            if ((i + 1) % width == 0) {
+                printf("\n");
+            }
         }
+    } else if (choice == 2) {
+        printf("\nEnter pixel values:\n");
+        for (i = 0; i < height * width; i++) {
+            scanf("%d", &intImg[i]);
+        }
+
+        printf("\nInput pixel values:\n");
+        for (i = 0; i < height * width; i++) {
+            printf("%d ", intImg[i]);
+            if ((i + 1) % width == 0) {
+                printf("\n");
+            }
+        }
+    } else {
+        printf("Invalid choice. Exiting.\n");
+        free(intImg);
+        free(floatImg);
+        return 1;
     }
 
-    // High-resolution timer variables
     LARGE_INTEGER frequency, start, end;
     double elapsed_time;
 
     QueryPerformanceFrequency(&frequency);
-
     QueryPerformanceCounter(&start);
 
     imgCvtGrayInttoFloat(height, width, intImg, floatImg);
 
     QueryPerformanceCounter(&end);
 
-    // Calculate elapsed time in nanoseconds
     elapsed_time = (double)(end.QuadPart - start.QuadPart) * 1e9 / frequency.QuadPart;
 
-    printf("\nInt to Float:\n\n");
+    printf("\nConverted Float Pixel Values:\n\n");
     for (i = 0; i < height; i++) {
         for (j = 0; j < width; j++) {
             printf("%.2f ", floatImg[i * width + j]);
